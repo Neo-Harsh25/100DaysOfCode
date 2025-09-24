2e59b42fcd34ab0f636988daac07009081706a0c
Neo-Harsh25 <neoharsh25@gmail.com>
2025-09-24 22:36:44 +0530

    Add files via upload

---
+++ Q67.c
@@ -0,0 +1,29 @@
+//Q67: Insert an element in an array at a given position.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+
+*/
+
+#include <stdio.h>
+int main() {
+    int n, i, pos, x;
+    scanf("%d", &n);
+    int a[n+1];
+    for(i=0; i<n; i++) scanf("%d", &a[i]);
+    scanf("%d %d", &pos, &x);
+
+    for(i=n; i>pos; i--) {
+        a[i] = a[i-1];
+    }
+    a[pos] = x;
+
+    for(i=0; i<=n; i++) printf("%d ", a[i]);
+    return 0;
+}
---
+++ Q69.c
@@ -0,0 +1,39 @@
+//Q69: Find the second largest element in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+10 20 30 40 50
+Output 1:
+40
+
+*/
+
+#include <stdio.h>
+int main() {
+    int n, i, max1, max2;
+    scanf("%d", &n);
+    int a[n];
+    for(i=0; i<n; i++) scanf("%d", &a[i]);
+
+    if(a[0] > a[1]) {
+        max1 = a[0];
+        max2 = a[1];
+    } else {
+        max1 = a[1];
+        max2 = a[0];
+    }
+
+    for(i=2; i<n; i++) {
+        if(a[i] > max1) {
+            max2 = max1;
+            max1 = a[i];
+        } else if(a[i] > max2 && a[i] != max1) {
+            max2 = a[i];
+        }
+    }
+
+    printf("%d", max2);
+    return 0;
+}
